eea6620c48bc4560b4a5a754d331e7afeacff65a
Daniel-Araya-Abera <danielaraya2191@gmail.com>
2022-04-15 13:28:36 +0300

    backend remove isAuthenticated for bldg detail

--- backend/controllers/buildingsController.js
+++ backend/controllers/buildingsController.js
@@ -20,7 +20,7 @@ router.get('/', async (req, res, next) => {
 });
 
 
-router.get('/:id', authMiddleware.isAuthenticated , async (req, res, next) => {
+router.get('/:id', async (req, res, next) => {
     try {
         
         const bldgId = req.params.id;
