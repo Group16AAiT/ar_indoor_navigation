bfc86a57140274efa623da297a61888af6d7c085
Daniel-Araya-Abera <danielaraya2191@gmail.com>
2022-04-15 12:29:38 +0300

    backend fix: heroku deploy

--- backend/middlewares/auth.js
+++ backend/middlewares/auth.js
@@ -4,7 +4,7 @@ const config = require('../config');
 
 const isAuthenticated = async(req, res, next) => {
     try {
-        const token = req.headers.authorization?.split(' ')[1]
+      const token = req.headers.authorization.split(' ')[1];
         if (!token) {
           return res.status(403).json({
               message: 'Invalid Authentication Credentials, token not provided',
