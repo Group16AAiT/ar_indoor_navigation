7a8e87a822531c9d1946936769b8f419737df024
Daniel-Araya-Abera <danielaraya2191@gmail.com>
2022-05-05 19:10:47 +0300

    [Backend] add change password

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -5,6 +5,7 @@ const User = require('../models/userModel')
 const jwt = require('jsonwebtoken')
 const config = require('../config')
 const bcrypt = require('bcryptjs')
+authMiddleware = require('../middlewares/auth');
 
 
 // Signup router
@@ -63,6 +64,58 @@ router.get('/logout', function (req, res) {
 
 });
 
+// Change password
+router.post('/change-password', authMiddleware.isAuthenticated, async (req, res) => {
+    try {
+        const user = await User.findOne({ _id: req.userId })
+        console.log({user});
+
+        if (!user) {
+            return res.status(404).json({
+                status: 'error',
+                code: 404,
+                message: "User does not exist"
+            });
+        }
+        if(req.body.oldPassword == null) {
+            return res.status(400).json({
+                status: 'error',
+                code: 400,
+                message: "old password not provided"
+            });
+        }
+        if(req.body.newPassword == null) {
+            return res.status(400).json({
+                status: 'error',
+                code: 400,
+                message: "New password not provided"
+            });
+        }
+        const validOldPassword = await bcrypt.compare(req.body.oldPassword, user.password);
+        if (!validOldPassword) {
+            return res.status(401).json({
+                status: 'error',
+                code: 401,
+                message: "Invalid old password"
+            });
+        }
+        const salt = await bcrypt.genSalt(10);
+        user.password = await bcrypt.hash(req.body.newPassword, salt);
+        await user.save();
+        res.status(200).json({
+            status: 'success',
+            code: 200,
+            message: "Password successfully changed"
+        });
+    } catch (e) {
+        console.log(e)
+        res.status(500).json({
+            status: 'error',
+            code: 500,
+            message: "Unknown error"
+        });
+    }
+});
 
 
 module.exports = router;
