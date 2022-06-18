b004e74ab2c06966481cd66bfb19cc0d2f0890ff
Daniel-Araya-Abera <danielaraya2191@gmail.com>
2022-06-19 02:16:27 +0300

    [Mobile] add SocketException check and edit error message to login_bloc

--- ar_indoor_nav_admin/lib/data/account/bloc/login/login_bloc.dart
+++ ar_indoor_nav_admin/lib/data/account/bloc/login/login_bloc.dart
@@ -1,8 +1,10 @@
 import 'dart:async';
+import 'dart:io';
 
 import 'package:ar_indoor_nav_admin/data/account/repository/account_repository.dart';
 import 'package:bloc/bloc.dart';
 import 'package:equatable/equatable.dart';
+import 'package:http/http.dart' as http;
 
 part 'login_event.dart';
 part 'login_state.dart';
@@ -20,9 +22,12 @@ class LoginBloc extends Bloc<LoginEvent, LoginState> {
       yield LoggingInState();
       await accountRepository.login(event.email, event.password);
       yield LoggedInState();
+    } on SocketException catch (e) {
+      yield const ErrorLoggingInState("some error happened while logging");
     } catch (e) {
       print(e);
-      yield const ErrorLoggingInState("some error happened while logging");
+      yield const ErrorLoggingInState("incorrect email or password");
+      // yield const ErrorLoggingInState("some error happened while logging");
     }
   }
 }
